28d25f4988c7ad9c11f1886c1340edb22f444b6b
Pedro <pedrolucasbastos071@gmail.com>
2023-05-30 17:36:49 -0300

    alterando chave ssh

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "k8s" {
   key_name   = "k8s-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKe1wePcgxVbOZZTTgQOwcMZ9UBrTnruJbitpmCxlMA8B1b/DOzPPbdCALj+JH7ujyay+yuKnPiZ0qu+GsFdbbc924yVI1W13FzJmAovmQXvt53Gr5LKDFUuEAgihQPZL8rrScPF16iKxpy1rOeJZKSaPUoZr+QqXzst3s+ZxEcY7Raj3Xb8MNJS4eKvdXYzUuYJJfNNbsa1xw1CNGlw24166R0aX0/NGZICcLYbnxK+7lB0lRB7q4LqNcvsid/kl6grLIwlaRU1tYhE46ZkEThU9nX3dxNGhNQ6jGGsIE8yFDqjnln47kjIdMdsepQj4c1L7RpzkER7r+DvRMdkrp pedro@pedro"
+  public_key = "sua-chave-ssh"
 }
 
 resource "aws_security_group" "k8s-sg" {
